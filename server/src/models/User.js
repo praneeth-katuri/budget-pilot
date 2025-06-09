86f62c7045072c1192392e6a8dca6edb6f64b8b8
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-06-09 21:41:52 +0530

    Fixed method from put to patch in userRoutes (#41)
    - added name field to the User Schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
+  name: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   monthlyBudget: { type: Number, default: 0 },
