241be36fe8c3ceaa6d06313843cc3a5afa234e17
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-06-09 19:55:07 +0530

    refactor: refactor App.js
    - moved routing component to seperate AppRoute component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,15 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import { useAuthStore } from "@/context/authStore";
-import DashboardPage from "@/pages/Dashboard";
-import Login from "@/pages/Auth/Login";
-import Register from "@/pages/Auth/Register";
-import AddSource from "@/pages/AddSource";
-import AddExpense from "@/pages/AddExpense";
-import Transfers from "@/pages/Transfers";
+import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
-
-function PrivateRoute({ children }) {
-  const { isAuthenticated, isLoading } = useAuthStore();
-  if (isLoading) return <p>Loading...</p>;
-  return isAuthenticated ? children : <Navigate to="/login" />;
-}
-
+import { useInitAuth } from "./hooks/useInitAuth";
+import { useAuthStore } from "./context/authStore";
 export default function App() {
+  const isLoading = useAuthStore((s) => s.isLoading);
+  useInitAuth();
+  if (isLoading) return null;
   return (
-    <Router>
+    <>
       <Toaster />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <DashboardPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/add-source"
-          element={
-            <PrivateRoute>
-              <AddSource />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/add-expense"
-          element={
-            <PrivateRoute>
-              <AddExpense />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/transfers"
-          element={
-            <PrivateRoute>
-              <Transfers />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/dashboard" />} />
-      </Routes>
-    </Router>
+      <AppRoutes />
+    </>
   );
 }
